3c60cf4dc30588e53415ceda1db4b4cff61c5ab5
Helder Souza <helder.souza@br.experian.com>
2022-05-18 22:46:11 -0300

    correções

--- Desafio.ME.Dominio/PedidoAprovacao.cs
+++ Desafio.ME.Dominio/PedidoAprovacao.cs
@@ -21,7 +21,7 @@ namespace Desafio.ME.Dominio
         public void AtribuirPedido(Pedido pedido) => Pedido = pedido;
         public void AtribuirItensAprovados(uint itensAprovados)
         {
-            if (itensAprovados == 0 && Status.Contains(PedidoStatus.REPROVADO))
+            if (itensAprovados == 0 && !Status.Contains(PedidoStatus.REPROVADO))
                 throw new ArgumentException("Para informar uma quantidade 0 o status do pedido deve ser REPROVADO.");
 
             ItensAprovados = itensAprovados;
@@ -31,7 +31,7 @@ namespace Desafio.ME.Dominio
             if (valorAprovado < 0)
                 throw new ArgumentException("Valor aprovado deve ser maior ou igual a 0.");
 
-            if (valorAprovado == 0 && Status.Contains(PedidoStatus.REPROVADO))
+            if (valorAprovado == 0 && !Status.Contains(PedidoStatus.REPROVADO))
                 throw new ArgumentException("Para informar um valor 0 o status do pedido deve ser REPROVADO.");
 
             ValorAprovado = valorAprovado;
